fde7b9885fcd7238f4f48a84f3c3d95e1cdfb28a
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-09-12 17:33:01 -0500

    First commit

---
+++ spec/models/card_spec.rb
@@ -0,0 +1,8 @@
+require_relative "../../app/models/card.rb"
+
+RSpec.describe 'Card' do
+  it 'has a type' do
+    card = Card.new('Ace of Spades')
+    expect(card.type).to eq('Ace of Spades')
+  end
+end
