2fae9c4c9bf9ecb8137a72c5c199091a81b6d438
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-09-12 18:45:11 -0500

    Modified all test to use before hook

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,13 +1,14 @@
 require_relative '../../app/models/card'
 
 RSpec.describe Card do
+  before do
+    @card = Card.new('Ace', 'Spades')
+  end
   it 'has a rank' do
-    card = Card.new('Ace', 'Spades')
-    expect(card.rank).to eq('Ace')
+    expect(@card.rank).to eq('Ace')
   end
 
   it 'has a suit' do
-    card = Card.new('Ace', 'Spades')
-    expect(card.suit).to eq('Spades')
+    expect(@card.suit).to eq('Spades')
   end
 end
--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -1,13 +1,14 @@
 require_relative '../../app/models/school'
 
 RSpec.describe School do
+  before do
+    @school = School.new('Beverly Hills High School')
+  end
   it 'has a name' do
-    school = School.new('Beverly Hills High School')
-    expect(school.name).to eq('Beverly Hills High School')
+    expect(@school.name).to eq('Beverly Hills High School')
   end
 
   it 'should start off with no students' do
-    school = School.new('Notre Dame High School')
-    expect(school.students).to eq([])
+    expect(@school.students).to eq([])
   end
 end
