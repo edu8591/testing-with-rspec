e314c6c1bb8b75ab211fba50fa991035ec6cb126
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-11-28 15:50:49 -0500

    predicate_mathcers

--- spec/models/18_predicate_matchers_spec.rb
+++ spec/models/18_predicate_matchers_spec.rb
@@ -1,3 +1,17 @@
 RSpec.describe 'predicate matchers' do
+  let(:result) { 16 / 2 }
 
+  it 'can be tested with ruby methods' do
+    expect(result.even?).to eq(true)
+  end
+
+  it 'can be tested with predicate matchers' do
+    # we can use all the other methods in ruby to test, we only append them to the be_
+    expect(result).to be_even
+    expect(result).not_to be_odd
+  end
+
+  describe 0 do
+    it { is_expected.to be_zero }
+  end
 end
