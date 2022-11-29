bbbcc331d85a66f0a6c24ed90ef0111d6d4218b4
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-11-29 13:13:43 -0500

    have attributes matcher

---
+++ spec/models/24_have_attribute_matcher_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../../app/models/professional_wrestler'
+
+RSpec.describe 'have attribute matcher' do
+  context ProfessionalWrestler.new('Stone Cold Steve Austin', 'Stunner') do
+    it 'checks for object attribute and proper values' do
+      expect(subject).to have_attributes(name: 'Stone Cold Steve Austin')
+      expect(subject).to have_attributes(name: 'Stone Cold Steve Austin', finishing_move: 'Stunner')
+    end
+
+    it { is_expected.to have_attributes(name: 'Stone Cold Steve Austin')}
+  end
+end
